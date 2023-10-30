b4e49870ce8d3918592abef0660d456cc5a0ed40
mahgoe <139371012+mahgoe@users.noreply.github.com>
2023-10-30 20:22:50 +0100

    changes to whole structure and index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const Mustache = require("mustache");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+const MUSTACHE_MAIN_DIR = "./main.mustache";
+
+let DATA = {
+  refresh_date: new Date().toLocaleDateString(`en-EN`, {
+    weekday: "long",
+    month: `long`,
+    day: `numeric`,
+    hour: `numeric`,
+    minute: `numeric`,
+    timeZoneName: `short`,
+    timeZone: `Europe/Zurich`,
+  }),
+};
+
+async function getWeather() {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=zuerich&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then((r) => r.json())
+    .then((r) => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+    });
+}
+
+async function getReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (ex, data) => {
+    if (ex) {
+      throw ex;
+    }
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync("README.md", output);
+  });
+}
+
+async function action() {
+  await getWeather();
+
+  await getReadMe();
+}
+
+action();
